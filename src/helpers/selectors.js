3c211584fc30ec821c79ef45110a9b542aa4eec4
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2021-12-13 18:31:32 -0600

    add getInterview func to return interview object

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,3 +16,16 @@ export function getAppointmentsForDay(state, dayName) {
 
   return result;
 }
+
+export function getInterview(state, interview) {
+
+  let result = null;
+
+  if (interview) {
+    let interviewerId = interview.interviewer;
+    result = { student: interview.student, interviewer: state.interviewers[interviewerId] };
+  }
+
+  return result;
+
+}

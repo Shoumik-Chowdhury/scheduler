80733a93ba7b979d9c7cf30f5d3a0dad4561950c
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2021-12-13 18:32:51 -0600

    add custom hook for transition and history

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const useVisualMode = (initialMode) => {
+
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newMode, replace = false) => {
+    
+    setMode(newMode);
+    
+    if (replace) {   
+      setHistory(prev => {
+        prev[prev.length - 1] = newMode;
+        return prev;
+      });    
+    }
+
+    if (!replace) {
+      setHistory(prev => ([...prev, newMode]));
+    }
+
+  };
+
+  const back = () => {
+
+    setHistory(prev => {
+
+      if (prev.length > 1) {
+        prev.pop();
+        setMode(prev[prev.length - 1]);
+      }
+      return prev;
+    }); 
+    
+  };
+
+  return { mode, transition, back };
+};
+
+export default useVisualMode;
